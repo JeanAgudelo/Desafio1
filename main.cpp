b75cec4853c7a9e26314e8abfc76d095a885929a
Jean <jean.agudelo@udea.edu.co>
2025-09-18 12:42:32 -0500

    commit para retificar el funcionamiento, no incluye la primera parte del desafio

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opcion;
+    cout<<"menu principal, elija el problema que deseaa ver"<<ednl;
+    cout<<"problema 1"<<endl;
+    cout<<"problema 2"<<endl;
+    cin>>opcion;
+    switch (opcion) {
+    case 1:
+        cout<<"Problema 2"<<endl;
+
+        break;
+    default:
+        break;
+    }
+}
