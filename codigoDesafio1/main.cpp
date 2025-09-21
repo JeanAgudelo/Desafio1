486b575158da406947beb1ce183b4e3f0aeb5c40
Jean <jean.agudelo@udea.edu.co>
2025-09-21 12:23:22 -0500

    Primer avance del desafio, inicio con un contadir de bits.
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

---
+++ codigoDesafio1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+unsigned ror(unsigned char r,int n){
+    return (unsigned char)((v>>n) | (v<<(8-n))));
+}
