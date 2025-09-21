0cd8624406e6425cd760f863b918892ee52f1746
Jean <jean.agudelo@udea.edu.co>
2025-09-21 12:40:07 -0500

    Empiezo funcion para el XOR.
    Signed-off-by: Jean <jean.agudelo@udea.edu.co>

--- codigoDesafio1/main.cpp
+++ codigoDesafio1/main.cpp
@@ -12,3 +12,10 @@ int main()
 unsigned ror(unsigned char r,int n){
     return (unsigned char)((v>>n) | (v<<(8-n))));
 }
+
+unsigned char* XOR(const unsigned char* enc,int sz,int n, unsigned char k){
+    unsigned char* out = new unsigned char[sz];
+    for(int i=0;i<sz;++i);
+       unsigned char b = enc[i];
+
+}
